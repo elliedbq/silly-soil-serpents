58d94f934cf023bfc170a30502715d1f2bb9bbf8
elliedbq <elliek803@yahoo.com>
2025-12-16 05:25:55 -0500

    complete serpentine movement code

--- testing/pico_motor.py
+++ testing/pico_motor.py
@@ -2,7 +2,7 @@ from machine import Pin, PWM
 import sys
 
 # Setup 5 servos on GPIO pins
-pins = [2, 3, 4, 5, 6, 7]
+pins = [0, 1, 2, 3, 4, 5]
 servos = [PWM(Pin(p)) for p in pins]
 for s in servos:
     s.freq(50)  # Standard servo frequency
@@ -10,10 +10,10 @@ for s in servos:
 def angle_to_duty(angle):
     # Map 0–180° to duty cycle (adjust for your servos)
     # clamp angle (avoid damage to servo)
-    angle = max(0, min(180, angle))
+    #angle = max(0, min(180, angle))
     # servo specific range (min = 0, max = 180)
     mini_range = 1000
-    max_range = 2000
+    max_range = 2600
     # convert angle to pulse width
     p_width = mini_range + (max_range - mini_range) * angle/180
     # convert pulse width to duty_u16
